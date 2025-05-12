9cfb2348b5c05883db41cb839898d9e03f2a0ecb
eli <lykkh78@gmail.com>
2025-05-12 15:54:28 +0300

    branch master

---
+++ Program.cs
@@ -0,0 +1,77 @@
+﻿using System.ComponentModel.Design;
+class Program()
+{
+    static void Main(string[] args)
+    {
+        
+
+        void menu()
+        {
+            List<int> series = new List<int>;
+        }
+
+        List<string> changeStrToLstr(string str)
+        {
+            
+            return ;
+        }
+
+        bool validationList(List<string> list)
+
+        {
+            return true;
+        }
+
+        List<int> changeLStrToLInt(List<string> list)
+        {
+            return list;
+        }
+
+        // Prints the menu
+        void printMenu()
+        {
+
+        }
+        List<int> reversedList(List<int> list)
+        {
+            return list;
+        }
+
+        List<int> soortedList(List<int> list)
+        {
+            return list;
+        }
+
+        int maxVal(List<int> list)
+        {
+            return 0;
+        }
+
+        int minVal(List<int> list)
+        {
+            return 0;
+        }
+
+        int avrageValues(List<int> list)
+        {
+            return 0;
+        }
+
+        int countVal(List<int> list)
+        {
+            return 0;
+        }
+
+        int sumAllVal(List<int> list)
+        {
+            return 0;
+        }
+
+
+
+
+    }
+
+}
+
+}
