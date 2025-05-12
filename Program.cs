14b386860939813e48b9e911178158a55d9a3e5a
eli <lykkh78@gmail.com>
2025-05-12 16:16:51 +0300

    adding function printMenu

--- Program.cs
+++ Program.cs
@@ -3,17 +3,17 @@ class Program()
 {
     static void Main(string[] args)
     {
-        
+        printMenu();
 
         void menu()
         {
-            List<int> series = new List<int>;
+            List<int> series = new List<int>();
         }
 
         List<string> changeStrToLstr(string str)
         {
-            
-            return ;
+            List<string> list = new List<string>();
+            return list;
         }
 
         bool validationList(List<string> list)
@@ -24,13 +24,24 @@ class Program()
 
         List<int> changeLStrToLInt(List<string> list)
         {
-            return list;
+            List<int> ints = new List<int>();
+            return ints;
         }
 
         // Prints the menu
         void printMenu()
         {
-
+            Console.WriteLine("Select one of the following options:\n" +
+                "a. Enter a new series of numbers (separate each number with a space)" +
+                "\nb. Print the current series of numbers" +
+                "\nc. Print the current series of numbers in reverse" +
+                "\nd. Print the current series of numbers sorted " +
+                "\ne. Print the highest value in the current series of numbers" +
+                "\nf. Print the lowest value in the current series of numbers" +
+                "\ng. Print the average of the current series of numbers" +
+                "\nh. Print the number of numbers in the series" +
+                "\ni. Print the sum of the total elements in the series" +
+                "\nj. Exit");
         }
         List<int> reversedList(List<int> list)
         {
@@ -74,4 +85,3 @@ class Program()
 
 }
 
-}
